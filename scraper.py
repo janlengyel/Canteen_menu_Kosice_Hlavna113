b91a8082b1998dddf5f4456f892092957be5f818
janlengyel <lengyel1999@gmail.com>
2018-09-20 18:56:23 +0200

    Small tweaks

--- scraper.py
+++ scraper.py
@@ -8,21 +8,26 @@ http = urllib3.PoolManager()
 request = http.request('GET', 'https://eskoly.sk/hlavna113/jedalen')
 if request.status == 200:
 
-    if not os.path.isfile("hash.txt"):
-        open("hash.txt", "w").close()
-
-    with open("hash.txt", "r") as text:
-        text = text.read()
-        if text:
-            hash = text
-            print("file:", hash)
-
     raw_data = request.data
     soup = BeautifulSoup(raw_data, "html.parser")
 
-    print("html:", md5(str(soup.findAll("tr")).encode('utf-8')).hexdigest())
+    original_hash=md5(str(soup.findAll("tr")).encode('utf-8')).hexdigest()
+    print("html:", original_hash)
 
-    if not hash == md5(str(soup.findAll("tr")).encode('utf-8')).hexdigest():
+    if not os.path.isfile("data.sqlite"):
+        open("data.sqlite", "w").close()
+
+    connection = sql3.connect("data.sqlite")
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS main(date TEXT, menu TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS versions(hash TEXT)")
+    connection.commit()
+
+    hash=cursor.execute("SELECT * FROM versions").fetchone()
+    if hash:hash=hash[0]
+    print("data:",hash)
+
+    if not hash == original_hash:
         print("Data difference found")
 
         res = []
@@ -35,18 +40,6 @@ if request.status == 200:
 
         for i in range(1, len(res), 2):
             res[i] = res[i][0:-1]
-        # for i in res:
-        # print(i)
-
-        # if os.path.exists("data.sqlite"):
-        #    os.remove("data.sqlite")
-        if not os.path.isfile("data.sqlite"):
-            open("data.sqlite", "w").close()
-
-        connection = sql3.connect("data.sqlite")
-        cursor = connection.cursor()
-        cursor.execute("CREATE TABLE IF NOT EXISTS main(date TEXT, menu TEXT)")
-        connection.commit()
 
         for i in range(0, len(res), 2):
             date = str(res[i][1]).replace(". ", "-")
@@ -60,8 +53,9 @@ if request.status == 200:
                 connection.commit()
                 print("Data for date", date, "were updated")
 
-        with open("hash.txt", "w") as text:
-            text.write(md5(str(soup.findAll("tr")).encode('utf-8')).hexdigest())
+        cursor.execute("INSERT INTO versions VALUES(?)",
+                       (md5(str(soup.findAll("tr")).encode('utf-8')).hexdigest(),))
+        connection.commit()
 
     else:
         print("Data are up to date")
